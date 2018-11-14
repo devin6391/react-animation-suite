a4576fbd291708da03acd288dd1994756e753971
Rishabh Jain <rishabh290992@gmail.com>
2018-11-14 11:40:57 +0530

    fix: zindex change in style

--- src/Slider/TransitioningComponent.tsx
+++ src/Slider/TransitioningComponent.tsx
@@ -211,16 +211,13 @@ export default class TransitioningComponent extends React.Component<
     }
   }
 
-  // Get the wrapper css style, non dependent on state of transition.
-  private get cssWrapperStyle(): React.CSSProperties {
-    const rtgWrapperStyles = getSliderStyles().rtgWrapper;
-    const zIndex = this.props.childStyles.zIndex;
-    const styles =  { ...rtgWrapperStyles, width: this.props.childStyles.width, height: this.props.childStyles.height };
-    if(zIndex) {
-      styles.zIndex = zIndex;
-    }
-    return styles;
-  }
+ // Get the wrapper css style, non dependent on state of transition.
+ private get cssWrapperStyle(): React.CSSProperties {
+  const rtgWrapperStyles = getSliderStyles().rtgWrapper;
+  const styles =  { ...rtgWrapperStyles, width: this.props.childStyles.width, height: this.props.childStyles.height };
+  const zIndex = this.props.childStyles.zIndex;
+  return zIndex ? {...styles, zIndex : zIndex } : styles;
+}
 
   // Abstraction of rendering child element. Used inside JSX rendering of child elem.
   private renderTransitioningChild(
